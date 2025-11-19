7601bc5a64839154d1c2098feed4d3f58bd5e91e
SpingleB <spinglebreadgit@gmail.com>
2025-11-19 15:58:10 +0200

    Done

--- Practice9/script.js
+++ Practice9/script.js
@@ -2,16 +2,16 @@ const svgInButtonElement = document.getElementById(`share-button-svg`);
 const svgPathElement = document.getElementById(`svg-path`);
 const buttonElement = document.getElementById(`share-btn`);
 const sharingDivElement = document.querySelector(`.sharing-div`);
+const sharingDivMobileElement = document.querySelector(`.sharing-div-mobile`);
 
 buttonElement.addEventListener("click", () => {
   svgPathElement.classList.toggle(`change-fill-in-btn`);
-});
-
-buttonElement.addEventListener("click", () => {
   buttonElement.classList.toggle(`change-color-in-btn`);
-  console.log(sharingDivElement);
+  if(innerWidth <= 768) {
+      sharingDivMobileElement.classList.toggle(`visible`);
+  } else if (innerWidth > 768){
+      sharingDivElement.classList.toggle(`visible`);
+  }
 });
 
-buttonElement.addEventListener("click", () => {
-  sharingDivElement.classList.toggle(`visible`);
-});
+
